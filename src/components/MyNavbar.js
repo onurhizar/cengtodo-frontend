358b40761da8a6ec262a255caa2088f4aa647ed2
Onur <onurhizar@tutanota.com>
2022-04-30 08:55:17 +0300

    feat: offcanvas added for navbar settings icon
    also site title changed to iyteceng

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -1,8 +1,14 @@
-import { Nav, Navbar, Container, Button } from "react-bootstrap"
+import { Fragment, useState } from "react"
+import { Nav, Navbar, Container, Button, Offcanvas } from "react-bootstrap"
 
 function MyNavbar(){
+    const [show, setShow] = useState(false); 
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
 
     return (
+        <Fragment>
+
         <Navbar bg="light">
             <Container>
             <Navbar.Brand href="#home">iyteceng</Navbar.Brand>
@@ -14,13 +20,25 @@ function MyNavbar(){
                     <Nav.Link href="#comments" disabled>yorumlar</Nav.Link>
                 </Nav>
                 <Nav>
-                    <Button variant="secondary">
+                    <Button variant="secondary" onClick={handleShow}>
                         <i class="bi bi-gear"></i>
                     </Button>
                 </Nav>
             </Navbar.Collapse>
             </Container>
         </Navbar>
+
+        <Offcanvas show={show} onHide={handleClose} placement="end">
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+            Some text as placeholder. In real life you can have the elements you
+            have chosen. Like, text, images, lists, etc.
+            </Offcanvas.Body>
+        </Offcanvas>
+
+        </Fragment>
     )
 }
 
